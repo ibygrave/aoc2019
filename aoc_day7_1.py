e663e5a6b8b245a28d90525c0f946ae4c138a3f2
Ian Bygrave <ian@bygrave.me.uk>
2020-01-19 11:27:23 +0000

    Day 7 part 1

---
+++ aoc_day7_1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import aocutils
+import aocintcode
+
+
+with open("day7_input.txt") as input_file:
+    prog = input_file.readline()
+
+print(aocintcode.optimize_amps(prog, [0,1,2,3,4]))
