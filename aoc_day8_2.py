0cade1e3ab183c7c100e89861bed58ddb2b5842f
Ian Bygrave <ian@bygrave.me.uk>
2020-01-20 23:27:32 +0000

    Day 8

---
+++ aoc_day8_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import aocsif
+
+i = aocsif.Image(25, 6)
+with open("day8_input.txt") as input_file:
+    i.input(input_file.readline().strip())
+print(i)
---
+++ aocsif.py
@@ -0,0 +1,40 @@
+import numpy
+
+class Image(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.layers = []
+    def input(self, data):
+        digits = (int(digit) for digit in data)
+        layer = []
+        for digit in digits:
+            layer.append(digit)
+            if len(layer) == self.width * self.height:
+                self.layers.append(layer)
+                layer = []
+        assert not layer
+    def digit_counts(self):
+        ret = []
+        for layer in self.layers:
+            a = numpy.array(layer)
+            unique, counts = numpy.unique(a, return_counts=True)
+            ret.append(dict(zip(unique, counts)))
+        return ret
+    def elf_check(self):
+        dc = self.digit_counts()
+        dc.sort(key=lambda ldc: ldc.get(0, 0))
+        return dc[0].get(1, 0) * dc[0].get(2, 0)
+    def __str__(self):
+        res = ''
+        for y in range(self.height):
+            for x in range(self.width):
+                res += {0:'0', 1:'1'}[self.colour(x, y)]
+            res += '\n'
+        return res
+    def colour(self, x, y):
+        layer_ix = (self.width * y) + x
+        for layer in self.layers:
+            if layer[layer_ix] == 2:
+                continue
+            return layer[layer_ix]
