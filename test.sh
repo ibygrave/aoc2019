7c26376b08d87705f4fcdd8e741ef602f09391c3
Ian Bygrave <ian@bygrave.me.uk>
2020-02-29 19:18:53 +0000

    Atomic replacement of regression answers.txt

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ set -ex
 . .testenv/bin/activate
 pytest
 
-for q in ./aoc_day*.py; do echo $q; python $q; done > answers.txt
+for q in ./aoc_day*.py; do echo $q; python $q; done > answers.txt.new
+mv answers.txt.new answers.txt
 
 echo DONE
