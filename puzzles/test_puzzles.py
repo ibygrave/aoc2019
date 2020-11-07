d124cf80f3723f069f70df233ccc597aa129375c
Ian Bygrave <ian@bygrave.me.uk>
2020-11-07 17:54:57 +0000

    Split puzzle regression tests from initial data generation

--- puzzles/test_puzzles.py
+++ puzzles/test_puzzles.py
@@ -3,23 +3,34 @@ import subprocess
 import pytest
 
 DATA_DIR=os.path.dirname(__file__)
+ALL_DAYS=list(range(1, 17))
 
 
-@pytest.mark.parametrize("day", range(1, 17))
-def test_puzzles(day):
-    input_data_file=os.path.join(DATA_DIR, f'day{day}_input.txt')
-    output_data_file=os.path.join(DATA_DIR, f'day{day}_output.txt')
+def input_file(day):
+    return os.path.join(DATA_DIR, f'day{day}_input.txt')
 
-    if os.path.exists(output_data_file):
-        with open(output_data_file, 'r') as f:
-            expected_output_data = f.read()
-    else:
-        expected_output_data = None
 
-    output_data = subprocess.check_output([f'aoc_day{day}', input_data_file]).decode('utf-8')
+def output_file(day):
+    return os.path.join(DATA_DIR, f'day{day}_output.txt')
 
-    if expected_output_data is None:
-        with open(output_data_file, 'w') as f:
-            f.write(output_data)
-    else:
-        assert output_data == expected_output_data
+
+@pytest.mark.parametrize("day", ALL_DAYS)
+def test_regression_puzzles(day):
+    if not os.path.exists(output_file(day)):
+        pytest.skip("no expected output data file")
+
+    with open(output_file(day), 'r') as f:
+        expected_output_data = f.read()
+
+    output_data = subprocess.check_output([f'aoc_day{day}', input_file(day)]).decode('utf-8')
+    assert output_data == expected_output_data
+
+
+@pytest.mark.parametrize("day", ALL_DAYS)
+def test_generate_puzzles(day):
+    if os.path.exists(output_file(day)):
+        pytest.skip("output data file already exists")
+
+    output_data = subprocess.check_output([f'aoc_day{day}', input_file(day)]).decode('utf-8')
+    with open(output_file(day), 'w') as f:
+        f.write(output_data)
